bade64e44835376d9ca246b2cb974afef7b9d530
Linesh Malkam <103716615+lmalkam@users.noreply.github.com>
2022-08-26 04:01:26 +0530

    commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,9 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # added manually
 
-STATIC_ROOT = "/var/www/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
