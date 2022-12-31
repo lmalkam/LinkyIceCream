6fb852079e0a1e77fde6b635a94c31bbd59c03a6
Linesh Malkam <103716615+lmalkam@users.noreply.github.com>
2022-12-31 15:23:44 +0530

    update

--- Hello/settings.py
+++ Hello/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wtrfvk#42w*=kk+t&kp+3)p8-932dh)*oe82n#&yirjgw$xq2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # '127.0.0.1:8000','icecreamlinky.herokuapp.com'
 
